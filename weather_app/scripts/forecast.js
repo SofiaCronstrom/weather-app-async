70c20ddfad9f0caa363332094df55e6385586329
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-09-07 17:30:20 +0200

    fetching weather from accuweather for current city in getWeather functio

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -1,5 +1,19 @@
 const key = 'ksDYGDYsPPqkX7WaxYf3RkujxBEqrbTZ';
 
+//fetch weather
+const getWeather = async (id) => {
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/'
+    const query = `${id}?apikey=${key}`;
+
+    const respons = await fetch(base + query);
+    const data = await respons.json();
+
+    return data[0];
+}
+
+
+
+//Fetch city 
 const getCity = async (city) => {
 
     const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -8,9 +22,11 @@ const getCity = async (city) => {
     const respone = await fetch(base + query);
     const data = await respone.json();
 
-    return(data[0]);
+    return data[0];
 };
 
-getCity('stockholm')
-.then( data => console.log(data))
-.catch(err => console.log(err));
+ getCity('stockholm').then( data => {
+    return getWeather(data.Key)
+ }).then(data => {
+    console.log(data);
+ }).catch(err => console.log(err));
