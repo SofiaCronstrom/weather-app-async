3f489dd49ba3b2dbae75793afd61a1e47a858fbe
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-09-08 12:06:22 +0200

    outputting the weather data in the DOM from updateUI function

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -1,15 +1,37 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
 
+const updateUI = (data) => {
+    
+    //saving data from updateCity in local variables
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    //Output details to the DOM
+    details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+      <span>${weather.Temperature.Metric.Value}</span>
+      <span>&deg;C</span>
+    </div>`;
+
+    //check if class d-none is showing on card
+   if (card.classList.contains('d-none')){
+       card.classList.remove('d-none')
+   }
+};
 
 const updateCity = async (city) => {
+
     //get the data from forecast.js
     const cityDets = await getCity(city);
     const weather = await getWeather(cityDets.Key);
+
     //return objects with data
-    return {
-        cityDets: cityDets,
-        weather: weather
-    };
+    //object shorthand notation
+    return {cityDets, weather};
 }
 cityForm.addEventListener('submit', e =>{
 e.preventDefault();
@@ -21,7 +43,7 @@ cityForm.reset();
 
 //console.log data from input and async 
 updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 
 });
